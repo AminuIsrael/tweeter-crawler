0c8950166c932ba3d2091ace1c400e71ac58f7c7
israel <aminuisrael2@gmail.com>
2020-08-10 20:21:47 +0000

    made changes

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
- 
 web: gunicorn app:flask_app
--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def fetch_tweets():
 
 
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0",port=9090, debug=True)
+    flask_app.run(port=9090, debug=True)
